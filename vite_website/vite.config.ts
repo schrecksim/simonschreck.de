5dd2a1cd041b1b468f68da5db335b238bfb4346e
Simon Schreck <sschreck00@proton.me>
2026-05-15 01:24:30 +0200

    0.1.1 minor adjustments

--- vite_website/vite.config.ts
+++ vite_website/vite.config.ts
@@ -2,15 +2,14 @@ import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import { intlayer, intlayerProxy } from "vite-intlayer";
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), intlayer(), intlayerProxy(), visualizer({
     emitFile: true,
     filename: "stats.html",
-  }), tailwindcss()],
+  }), ],
   preview: {
-    allowedHosts: ["0.0.0.0", "localhost", "127.0.0.1", "l6j8d401tdqnr1y80i62nepe.95.216.220.168.sslip.io",".simonschreck.de"],
+    allowedHosts: ["0.0.0.0", "localhost", "127.0.0.1", ".simonschreck.de"],
   }
 });
