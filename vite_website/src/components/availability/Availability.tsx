9f16795b2a296ba3abfab3a6b698edf58176bf2b
Simon Schreck <sschreck00@proton.me>
2026-05-17 20:15:43 +0200

    0.1.3 edited availability text

--- vite_website/src/components/availability/Availability.tsx
+++ vite_website/src/components/availability/Availability.tsx
@@ -5,13 +5,7 @@ const Availability = () => {
     return (
         <div>
             <h2>{content.h_availability}</h2>
-
             <p>{content.t_availability}</p>
-            <ul>
-                <li>{content.availability}</li>
-                <li>{content.main_area}</li>
-            </ul>
-
             <p>{content.disclaimer}</p>
         </div>
     );
