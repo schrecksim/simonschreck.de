d2b9c8d2e42c2e105d62d3e0a1d303525ba2bbb1
Simon Schreck <82154632+schrecksim@users.noreply.github.com>
2026-05-19 22:41:58 +0200

    Update vite.config.ts

--- vite_website/vite.config.ts
+++ vite_website/vite.config.ts
@@ -2,11 +2,10 @@ import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import { intlayer, intlayerProxy } from "vite-intlayer";
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), intlayer(), intlayerProxy(), tailwindcss(), visualizer({
+  plugins: [react(), intlayer(), intlayerProxy(), visualizer({
     emitFile: true,
     filename: "stats.html",
   }), ],
