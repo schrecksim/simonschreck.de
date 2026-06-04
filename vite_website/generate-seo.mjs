937afc356d10f539e3804a2a006b729394def870
Simon Schreck <sschreck00@proton.me>
2026-06-04 18:59:01 +0900

    0.2.3 adjusted seo file(s)

--- vite_website/generate-seo.mjs
+++ vite_website/generate-seo.mjs
@@ -12,10 +12,15 @@ const SITE_URL = (process.env.SITE_URL || "https://simonschreck.de").replace(
 
 const pathList = [
     { path: "/", changefreq: "monthly", priority: 1.0 },
-    { path: "/aboutme", changefreq: "monthly", priority: 0.7 },
-    { path: "/glossary", changefreq: "monthly", priority: 0.7 },
-    { path: "/legalnote", changefreq: "monthly", priority: 0.7 },
-    { path: "/privacypolicy", changefreq: "monthly", priority: 0.7 },
+    { path: "/aboutme", changefreq: "monthly", priority: 0.8 },
+    { path: "/glossary", changefreq: "weekly", priority: 0.6 },
+    { path: "/glossary/business_intelligence", changefreq: "monthly", priority: 0.6 },
+    { path: "/glossary/data_analysis", changefreq: "monthly", priority: 0.6 },
+    { path: "/glossary/data_engineering", changefreq: "monthly", priority: 0.6 },
+    { path: "/glossary/data_science", changefreq: "monthly", priority: 0.6 },
+    { path: "/glossary/requirements_engineering", changefreq: "monthly", priority: 0.6 },
+    { path: "/legalnote", changefreq: "monthly", priority: 0.6 },
+    { path: "/privacypolicy", changefreq: "monthly", priority: 0.6 },
 ];
 
 const sitemapXml = generateSitemap(pathList, { siteUrl: SITE_URL });
@@ -24,7 +29,7 @@ fs.writeFileSync(path.join(__dirname, "public", "sitemap.xml"), sitemapXml);
 const getAllMultilingualUrls = (urls) =>
     urls.flatMap((url) => Object.values(getMultilingualUrls(url)));
 
-const allowedPaths = getAllMultilingualUrls(["/", "/aboutme", "/glossary", "/legalnote", "/privacypolicy"]);
+const allowedPaths = getAllMultilingualUrls(["/", "/aboutme", "/glossary", "glossary/business_intelligence", "glossary/data_analysis", "glossary/data_engineering", "glossary/data_science", "glossary/requirements_engineering", "/legalnote", "/privacypolicy"]);
 const disallowedPaths = getAllMultilingualUrls(["/admin", "/private"]);
 
 const robotsTxt = [
